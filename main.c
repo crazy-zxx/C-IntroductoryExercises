9f2ff476a34c1380cd2eaccaf5c5b73b3afaca58
xylx <1934109821@qq.com>
2021-02-21 18:14:55 +0800

    update

--- main.c
+++ main.c
@@ -2720,7 +2720,37 @@ void testSubstrTimes(){
 /*
  * 56、【题目】
  */
+typedef struct{
+    int ID;
+    int math;
+    int English;
+    int C;
+    int avargrade;
+    char name[20];
+}Stu;
 
+void testWriteFile(){
+    FILE*fp;
+    Stu stu[3];
+    int i,avargrade=0;
+    printf("请输入3个同学的信息：学生号，姓名，3门成绩:\n");
+    for(i=0;i<3;i++)
+    {
+        scanf("%d %s %d %d %d",&(stu[i].ID),stu[i].name,&(stu[i].math),&(stu[i].English),&(stu[i].C));
+        stu[i].avargrade=(stu[i].math+stu[i].English+stu[i].C)/3;
+    }
+
+    if((fp=fopen("stud.txt","w"))==NULL)
+    {
+        printf("error :cannot open file!\n");
+        exit(0);
+    }
+    for(i=0;i<3;i++)
+        fprintf(fp,"%d %s %d %d %d %d\n",stu[i].ID,stu[i].name,stu[i].math,stu[i].English,
+                stu[i].C,stu[i].avargrade);
+
+    fclose(fp);
+}
 
 
 
@@ -2830,7 +2860,8 @@ int main() {
     //testEncryptTelegram();
     //testTime();
     //guessNumber();
-    testSubstrTimes();
+    //testSubstrTimes();
+    testWriteFile();
 
 
     printf("\n运行耗时：");
